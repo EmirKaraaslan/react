d139999cae531940298d921eda5317f98d6306cb
Emir Karaaslan <96197752+EmirKaraaslan@users.noreply.github.com>
2024-08-13 15:27:39 +0300

    Update firebase.tsx

--- src/components/firebase/firebase.tsx
+++ src/components/firebase/firebase.tsx
@@ -11,14 +11,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCyeNjI-GxDdsVJSvOA8GEQorN0UWYlobE",
-  authDomain: "mywork-db.firebaseapp.com",
-  databaseURL: "https://mywork-db-default-rtdb.firebaseio.com",
-  projectId: "mywork-db",
-  storageBucket: "mywork-db.appspot.com",
-  messagingSenderId: "431892570255",
-  appId: "1:431892570255:web:4536694271b8c31ff490fa",
-  measurementId: "G-FRZQSRDLDQ"
+ 
 };
 
 // Initialize Firebase
